951e14e232ce6b6af12b4763cbf8a202b36fb979
Fabrizio A <technofab@users.noreply.github.com>
2017-01-23 18:29:18 +0100

    Created installhttpd.sh

---
+++ installhttpd.sh
@@ -0,0 +1,15 @@
+echo "Installing Apache + PHP + MOD_SSL + XCache"
+sleep 5
+screen -m yum install httpd mod_ssl php php-gd php-pear php-xcache xcache-admin -y
+mkdir /etc/httpd/sites-available
+mkdir /etc/httpd/sites-enabled
+systemctl start httpd
+systemctl enable httpd
+cd /etc/httpd/sites-available
+wget https://raw.githubusercontent.com/technofab/newcentos7/master/example.com.conf
+screen yum install python-certbot-apache -y
+clear
+echo "Ending first phase. If you also MySQL do:"
+echo "wget -O - https://raw.githubusercontent.com/technofab/newcentos7/master/installmysql.sh | bash"
+echo "else refer to general-management.md on newcentos7 on technofab github repository."
+sleep 5
