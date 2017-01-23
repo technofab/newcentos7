dd07a3266abe74138c0572b0066d6a43d1b9889a
Fabrizio A <technofab@users.noreply.github.com>
2017-01-23 15:55:13 +0100

    Update to latest epel rpm

--- installbase.sh
+++ installbase.sh
@@ -6,6 +6,7 @@ yum clean all
 yum install http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm -y
 yum install expect mosh screen yum-plugin-remove-with-leaves yum-plugin-show-leaves yum-utils mlocate nano iperf3 traceroute net-tools git zip unzip http://li.nux.ro/download/nux/dextop/el6/i386/screenie-1.30.0-5.el6.nux.noarch.rpm -y
 yum clean all
+echo "Updating the system to latest release"
 yum update -y
 echo "Installing Apache + PHP + MOD_SSL + XCache"
 yum install httpd mod_ssl php php-gd php-pear php-xcache xcache-admin -y
@@ -16,6 +17,7 @@ systemctl enable httpd
 cd /etc/httpd/sites-available
 wget https://raw.githubusercontent.com/technofab/newcentos7/master/example.com.conf
 yum install python-certbot-apache
-echo "If you need MySQL do:"
+echo "If you also MySQL do:"
 echo "wget -O - https://raw.githubusercontent.com/technofab/newcentos7/master/installmysql.sh | bash"
+echo "else refer to general-management.md on newcentos7 on technofab github repository."
 sleep 3
