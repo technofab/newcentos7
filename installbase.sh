36806744aa96a102bb40f03a4e0b8e0564ddeec8
Fabrizio A <technofab@users.noreply.github.com>
2017-01-23 17:41:51 +0100

    Update installbase.sh

--- installbase.sh
+++ installbase.sh
@@ -2,16 +2,16 @@
 echo "Installing base things..."
 systemctl stop firewalld
 systemctl disable firewalld
-sleep 3
+sleep 5
 yum clean all
 yum install screen http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm -y
 screen -m yum install expect mosh yum-plugin-remove-with-leaves yum-plugin-show-leaves yum-utils mlocate nano iperf3 traceroute net-tools git zip unzip http://li.nux.ro/download/nux/dextop/el6/i386/screenie-1.30.0-5.el6.nux.noarch.rpm -y
 yum clean all
 echo "Updating the system to latest release"
-sleep 3
+sleep 5
 screen -m yum update -y
 echo "Installing Apache + PHP + MOD_SSL + XCache"
-sleep 3
+sleep 5
 screen -m yum install httpd mod_ssl php php-gd php-pear php-xcache xcache-admin -y
 mkdir /etc/httpd/sites-available
 mkdir /etc/httpd/sites-enabled
@@ -24,4 +24,4 @@ clear
 echo "Ending first phase. If you also MySQL do:"
 echo "wget -O - https://raw.githubusercontent.com/technofab/newcentos7/master/installmysql.sh | bash"
 echo "else refer to general-management.md on newcentos7 on technofab github repository."
-sleep 3
+sleep 5
