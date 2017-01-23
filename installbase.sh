38c1a4eb25c71f416ce233aa78683c024e1b429f
Fabrizio A <technofab@users.noreply.github.com>
2017-01-23 15:23:43 +0100

    Updated installbase.sh

--- installbase.sh
+++ installbase.sh
@@ -3,7 +3,7 @@ echo "Installing base things..."
 systemctl stop firewalld
 systemctl disable firewalld
 yum clean all
-yum install http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm -y
+yum install http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm -y
 yum install expect mosh screen yum-plugin-remove-with-leaves yum-plugin-show-leaves yum-utils mlocate nano iperf3 traceroute net-tools git zip unzip http://li.nux.ro/download/nux/dextop/el6/i386/screenie-1.30.0-5.el6.nux.noarch.rpm -y
 yum clean all
 yum update -y
@@ -32,12 +32,11 @@ send \"y\r\"
 expect eof
 ")
 echo "$SECURE_MYSQL"
-sleep 5
 echo "Installing Apache + PHP + MOD_SSL + XCache"
 yum install httpd mod_ssl php php-gd php-pear php-xcache xcache-admin -y
 mkdir /etc/httpd/sites-available
 mkdir /etc/httpd/sites-enabled
 systemctl start httpd
 systemctl enable httpd
-wget example.com.conf
+wget https://raw.githubusercontent.com/technofab/newcentos7/master/example.com.conf
 yum install python-certbot-apache
