91a015675c3b5e225d3c8fe7e5cdb8293e0645ec
Fabrizio A <technofab@users.noreply.github.com>
2017-01-24 14:06:47 +0100

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-yum clean all; yum install wget -y
-cd /root
-wget https://raw.githubusercontent.com/technofab/newcentos7/master/installbase.sh 
-chmod 755 installbase.sh
-/root/installbase.sh
