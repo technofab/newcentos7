c6d57f9520f0fc99c0b35616488a4d5ef0a9e431
Fabrizio A <technofab@users.noreply.github.com>
2017-01-25 08:27:48 +0100

    Update installhttpd.sh

--- installhttpd.sh
+++ installhttpd.sh
@@ -9,8 +9,4 @@ systemctl enable httpd
 cd /etc/httpd/sites-available
 wget https://raw.githubusercontent.com/technofab/newcentos7/master/example.com.conf
 screen yum install python-certbot-apache -y
-clear
-echo "Ending first phase. If you also MySQL do:"
-echo "wget -O - https://raw.githubusercontent.com/technofab/newcentos7/master/installmysql.sh | bash"
-echo "else refer to general-management.md on newcentos7 on technofab github repository."
 sleep 5
